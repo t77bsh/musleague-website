97cffb0cb35c219e8a0a604a60caf847cbf40b96
t77bsh <taabish_khan22@hotmail.co.uk>
2023-09-30 19:19:03 +0100

    fixed margins

--- src/app/_components/Headings.tsx
+++ src/app/_components/Headings.tsx
@@ -4,12 +4,12 @@ export default function Headings() {
   return (
     <section id="headings" className={alegreya.className + " " + "mt-[4vh]"}>
       {/* Headlines */}
-      <div className="flex flex-col gap-y-1 sm:gap-y-4 font-semibold">
+      <div className="flex flex-col gap-y-4 font-semibold">
         <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
           A marriage, not a dating, app.
         </h1>
         <p className={"sm:text-lg md:text-xl lg:text-2xl"}>
-          ...for Muslim professionals <b className="text-[#c5b385]">serious</b>{" "}
+          For Muslim professionals <b className="text-[#c5b385]">serious</b>{" "}
           about marriage.
         </p>
       </div>
